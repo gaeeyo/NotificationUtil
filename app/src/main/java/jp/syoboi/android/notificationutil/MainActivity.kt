c151445468b59868bbfc813ae41ed0b463c02af5
gaeeyo <iknnofm0126@gmail.com>
2017-07-22 10:38:51 +0900

    事前の設定確認

--- app/src/main/java/jp/syoboi/android/notificationutil/MainActivity.kt
+++ app/src/main/java/jp/syoboi/android/notificationutil/MainActivity.kt
@@ -1,14 +1,7 @@
 package jp.syoboi.android.notificationutil
 
 import android.app.Activity
-import android.app.NotificationManager
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
-import android.support.v4.app.NotificationCompat
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,8 +11,13 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        testNotification.setOnClickListener { testNotification(this) }
+
         readNotificationSetting.paint.isUnderlineText = true
-        readNotificationSetting.setOnClickListener { startNotificationListenerSettingActivity() }
+        readNotificationSetting.setOnClickListener { startNotificationListenerSettingActivity(this) }
+
+        drawOverlaySetting.paint.isUnderlineText = true
+        drawOverlaySetting.setOnClickListener { startManageOverlayPermissionActivity(this) }
 
         popupNotification.setOnCheckedChangeListener { compoundButton, b ->
             updateViews()
@@ -35,58 +33,12 @@ class MainActivity : Activity() {
 
     fun updateViews() {
         readNotificationSetting.visibility =
-                if (isEnabledReadNotification) View.GONE else View.VISIBLE
+                if (isEnabledReadNotification(this)) View.GONE else View.VISIBLE
+
+        drawOverlaySetting.visibility =
+                if (canDrawOverlays(this)) View.GONE else View.VISIBLE
+
         popupNotificationGroup.visibility =
                 if (popupNotification.isChecked) View.VISIBLE else View.GONE
-    }
-
-    private val isEnabledReadNotification: Boolean
-        get() {
-            val contentResolver = contentResolver
-            val rawListeners = Settings.Secure.getString(contentResolver,
-                    "enabled_notification_listeners")
-            if (rawListeners == null || "" == rawListeners) {
-                return false
-            } else {
-                val listeners = rawListeners.split(":".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()
-                for (listener in listeners) {
-                    if (listener.startsWith(packageName)) {
-                        return true
-                    }
-                }
-            }
-            return false
-        }
-
-    private val canDrawOverlays: Boolean
-        get() {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                return Settings.canDrawOverlays(this)
-            } else {
-                return true
-            }
-        }
-
-
-    fun testNotification() {
-        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val n = NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentText("テストの通知です")
-                .setContentTitle("テスト")
-                .build()
-        nm.notify(1, n)
-    }
-
-    private fun startNotificationListenerSettingActivity() {
-        val intent = Intent()
-        intent.action = Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS
-        startActivity(intent)
-    }
-
-    private fun startManageOverlayPermissionActivity() {
-        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:${getPackageName()}"));
-        startActivityForResult(intent, 0)
     }
 }
---
+++ app/src/main/java/jp/syoboi/android/notificationutil/Utils.kt
@@ -0,0 +1,63 @@
+package jp.syoboi.android.notificationutil
+
+import android.app.Activity
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.support.v4.app.NotificationCompat
+
+
+fun isEnabledReadNotification(context: Context): Boolean {
+    val orgListeners = Settings.Secure.getString(context.contentResolver,
+            "enabled_notification_listeners")
+    if (orgListeners.isNullOrEmpty()) {
+        return false
+    } else {
+        val listeners = orgListeners.split(":").map { it.substringBefore("/") }
+        return listeners.contains(context.packageName)
+    }
+}
+
+fun canDrawOverlays(context: Context): Boolean {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        return Settings.canDrawOverlays(context)
+    } else {
+        return true
+    }
+}
+
+
+fun testNotification(context: Context) {
+    val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    val messagingStyle = NotificationCompat.MessagingStyle("テスト通知のユーザー名").apply {
+        addMessage("テスト通知のメッセージ", System.currentTimeMillis(), "テスト通知の送信者")
+        conversationTitle = "テスト通知のタイトル"
+    }
+
+    val n = NotificationCompat.Builder(context)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentText("テスト通知のテキストです")
+            .setContentTitle("テスト通知のタイトルです")
+            .setTicker("テストの通知のティッカーです")
+            .setAutoCancel(true)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setStyle(messagingStyle)
+            .build()
+    nm.notify(1, n)
+}
+
+fun startNotificationListenerSettingActivity(context: Activity) {
+    val intent = Intent()
+    intent.action = Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS
+    context.startActivity(intent)
+}
+
+fun startManageOverlayPermissionActivity(context: Activity) {
+    val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            Uri.parse("package:${context.getPackageName()}"));
+    context.startActivityForResult(intent, 0)
+}
